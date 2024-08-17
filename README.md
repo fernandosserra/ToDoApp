a00907868893d2663d83acd982927e123beb291a
Fernando Serra <fs.serra@outlook.com>
2024-08-16 23:03:12 -0300

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # ToDoApp
 A simple application to manage tasks
+
+## Index
+- Introduction
+- Technologies
+- Features
+- Installing
+- Using
+- License
+
+## Introduction
+This is a simple project, made for educational purposes, where the user can track their daily tasks and add them to two specific groups, or view all of them on a single screen.
+
+## Technologies
+The following technologies were used:
+- Python 3.12 as the main language
+- Flet as the framework
+- SQLite 3 as the database
+- Codeium Visual Studio Extension (to create Docstrings)
+
+## Features
+- Task Creation
+- Task Update
+
+## Installing
+
+## Using
+
+## License
+This project uses the MIT License.
+
+## Roadmap
+- 2024-08-16: Initial Release
+- 2024-09-30: I plan to add an option to delete notes
+- 2024-10-30: Add a new logo and background image
+
+## Screenshots
+<img src="D:\Projetos-Fernando\Python\ToDoApp\MainPage.png"/>
+<img src="D:\Projetos-Fernando\Python\ToDoApp\Adding_Task.png"/>
+<img src="D:\Projetos-Fernando\Python\ToDoApp\Current_Tasks.png"/>
+<img src="D:\Projetos-Fernando\Python\ToDoApp\Completed_Tasks.png"/>
---
+++ todo.py
@@ -0,0 +1,154 @@
+import flet as ft
+import sqlite3
+
+class ToDo:
+    def __init__(self, Page: ft.Page):
+        """
+        Initializes a new instance of the ToDo class with the specified Page.
+
+        Args:
+            Page (ft.Page): The Page to be used for the ToDo instance.
+
+        Returns:
+            None
+        """
+        self.Page = Page
+        self.Page.bgcolor = ft.colors.WHITE
+        self.Page.window.width = 350
+        self.Page.window.height = 450
+        self.Page.window.resizable = False
+        self.Page.window.always_on_top = True
+        self.Page.title = "To-Do App"
+        self.task = ''
+        self.view = 'all'
+        self.db_execute('CREATE TABLE IF NOT EXISTS tasks (name, status)')
+        self.results = self.db_execute('SELECT * FROM tasks')        
+        self.main_Page()
+
+    def checked(self, e):
+        is_checked = e.control.value
+        label = e.control.label
+        
+        if is_checked:
+            self.db_execute('UPDATE tasks SET status = ? WHERE name = ?',  params=["complete", label])
+        else:
+            self.db_execute('UPDATE tasks SET status = ? WHERE name = ?', params=["incomplete", label])
+
+        if self.view == 'all':
+            self.results = self.db_execute('SELECT * FROM tasks')
+        else:
+            self.results = self.db_execute('SELECT * FROM tasks WHERE status = ?', params=[self.view])
+            
+        self.update_task_list()
+        self.Page.update()
+    
+    def tasks_container(self):
+        """
+        Returns a container for tasks with a height of 80% of the Page height.
+        
+        The container includes a column with a checkbox control labeled "Tarefa 1" 
+        that is initially checked.
+        
+        Parameters:
+            self (ToDo): The instance of the ToDo class.
+        
+        Returns:
+            ft.Container: A container with the specified height and content.
+        """
+        return ft.Container(
+            height=self.Page.height * 0.8,
+            content = ft.Column(
+                controls = [
+                    ft.Checkbox(
+                        label = res[0],
+                        on_change = self.checked, 
+                        value = True if res[1] == 'complete' else False
+                        )
+                    for res in self.results if res                    
+                ]
+            )
+        )
+
+    def db_execute(self, query, params = []):
+        with sqlite3.connect('todo.db') as conn:
+            cur = conn.cursor()
+            cur.execute(query, params)
+            conn.commit()
+            return cur.fetchall()
+    
+    def set_value(self, e):
+        self.task = e.control.value
+
+    def update_task_list(self):
+        tasks = self.tasks_container()
+        self.Page.controls.pop()
+        self.Page.add(tasks)
+        self.Page.update()
+        
+    def add_task(self, e, input_task):
+        task_name = self.task
+        status = 'incomplete'
+        
+        if task_name:
+            self.db_execute(
+                query='INSERT INTO tasks VALUES (?, ?)',
+                params=(task_name, status)
+                )
+            input_task.value = ''
+            self.results = self.db_execute('SELECT * FROM tasks')
+            self.update_task_list()
+    
+    def tabs_changed(self, e):
+        if e.control.selected_index == 0:
+            self.results = self.db_execute('SELECT * FROM tasks')
+            self.view = 'all'
+        elif e.control.selected_index == 1:
+            self.results = self.db_execute('SELECT * FROM tasks WHERE status = ?', params=['incomplete'])
+            self.view = 'incomplete'
+        elif e.control.selected_index == 2:
+            self.results = self.db_execute('SELECT * FROM tasks WHERE status = ?', params=['complete'])
+            self.view = 'complete'
+        
+        self.update_task_list()
+    
+    def main_Page(self):
+        """
+        Initializes the main Page of the application.
+
+        This function sets up the main Page of the application by creating and adding the necessary components.
+
+        Parameters:
+            self (ToDo): The instance of the ToDo class.
+
+        Returns:
+            None
+        """
+        input_task = ft.TextField(
+            hint_text = "Digite aqui uma tarefa", 
+            expand = True,
+            on_change = self.set_value
+            )
+        input_bar = ft.Row(
+            controls = [input_task,
+                        ft.FloatingActionButton(
+                            icon = ft.icons.ADD, 
+                                on_click = lambda e: self.add_task(e, input_task)
+                                )
+                        ]
+        )
+        
+        tabs = ft.Tabs(
+            selected_index = 0,
+            on_change = self.tabs_changed,
+            tabs = [
+                ft.Tab(text = "Todos"),
+                ft.Tab(text = "Em andamento"),
+                ft.Tab(text = "Finalizados")
+            ]
+        )
+        
+        tasks = self.tasks_container()
+        
+        self.Page.add(input_bar, tabs, tasks)
+        
+ft.app(target=ToDo)
